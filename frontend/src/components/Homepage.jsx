ae8d9e21a863adfb2a3d4d67e63810c7dc61feca
Batoul-Alsayyed <batoul.alsayyed20@gmail.com>
2022-07-08 01:01:20 +0300

    Frontend Adding booknow and footer components to the home page

--- frontend/src/components/Homepage.jsx
+++ frontend/src/components/Homepage.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Header from '../components/Header';
 import StudentsSection from './StudentsSection';
+import BookNow from './BookNow';
+import Footer from './Footer';
 export default function Homepage() {
   return (
     <div>
       <Header/>
       <StudentsSection/>
+      <BookNow/>
+      <Footer/>
     </div>
   )
 }
