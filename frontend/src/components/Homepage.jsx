0a766c62fa674d18e1573b092fcf61806bda7189
Batoul-Alsayyed <batoul.alsayyed20@gmail.com>
2022-07-07 19:53:18 +0300

    Frontend Adding homepage component

---
+++ frontend/src/components/Homepage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Header from '../components/Header';
+import StudentsSection from './StudentsSection';
+export default function Homepage() {
+  return (
+    <div>
+      <Header/>
+      <StudentsSection/>
+    </div>
+  )
+}
