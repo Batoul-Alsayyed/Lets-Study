d8f8234f32ef43efa2f965a8cc399f9b21f70081
Batoul-Alsayyed <batoul.alsayyed20@gmail.com>
2022-07-07 23:46:37 +0300

    Adding book now section component

---
+++ frontend/src/components/BookNow.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import img5 from '../images/windows-p74ndnYWRY4-unsplash 1.png';
+import WhiteButton from './WhiteButton';
+import '../index.css'
+export default function BookNow() {
+  return (
+    <div className="header-container">
+    <div className="left-side">
+            <div className="header-left-side-components">
+                <img src={img5} alt="" />
+            </div>
+        </div>
+        <div className="right-side">
+            <div className="right-display">
+            <p className='p-style'>Book a session with either a student or with a professional tutor</p>
+            <div className="white-btn">
+            <WhiteButton className='btn' content={"Book now"}>
+            </WhiteButton>
+            </div>
+            </div>
+        </div>
+    </div>
+  )
+}
