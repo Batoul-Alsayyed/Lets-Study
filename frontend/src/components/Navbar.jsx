e27c6eaede610fc42ca16dc6417cc33a962ef40d
Batoul-Alsayyed <batoul.alsayyed20@gmail.com>
2022-07-07 16:35:38 +0300

    Frontend Adding navbar component

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
-
+import React from "react";
+import "../index.css";
+import logo from '../images/logo/logo.png';
 export default function Navbar() {
   return (
-    <div>
-      nav bar
+    <div className="navbar">
+        <div className="navbar-container">
+            <div className="left-side">
+                <div className="left-side-components">
+                    <img src={logo} alt="logo"/>
+                    <p>Lets Study</p>
+                </div>
+            </div>
+            <div className="right-side">
+                <div className="right-side-components">
+                    <p>Home</p>
+                    <p>Register</p>
+                    <p>Login</p>
+                </div>
+            </div>
+        </div>
     </div>
-  )
+  );
 }
