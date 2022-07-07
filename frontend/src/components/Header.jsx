1719fc85f0f4c18558568b49c99161c6ae78c59c
Batoul-Alsayyed <batoul.alsayyed20@gmail.com>
2022-07-07 18:44:27 +0300

    Adding header component

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import Navbar from '../components/Navbar';
+import "../index.css";
+import header from  '../images/header.png'
+
+export default function Header() {
+  return (
+    <div>
+        <Navbar/>   
+        <div className="header-container">
+        <div className="left-side">
+                <div className="header-left-side-components">
+                    <p className='header-p'>First Online Study Platform</p>
+                    <p className='start-p'>Start your study journey with students
+                        <br/> just like you or with a professional<br/> tutor.</p>
+                    <div className="buttons-styling">
+                    <button className='button1'>Join as a pro member</button>
+                    <button className='button2'>Study for free</button>
+                    </div>
+
+                </div>
+            </div>
+            <div className="right-side">
+                <div className="header-right-side-components">
+                    <img src={header} />
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
