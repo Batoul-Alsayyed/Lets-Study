2b648da16a341e1e652a24e1f83ed09be28ea14f
Batoul-Alsayyed <batoul.alsayyed20@gmail.com>
2022-07-07 18:44:59 +0300

    Adding header route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route, Link, BrowserRouter, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Header from './components/Header'
 function App() {
   return (
       <div>
         <BrowserRouter>
         <Routes>
           <Route path='/navbar' element={<Navbar/>}></Route>
+          <Route path='/header' element={<Header/>}></Route>
         </Routes>
         </BrowserRouter>
       </div>
