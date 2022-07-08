8db6aa4458b44f6cb423a4426946a15c668e654e
Batoul-Alsayyed <batoul.alsayyed20@gmail.com>
2022-07-09 00:08:13 +0300

    Frontend Adding signin component

---
+++ frontend/src/components/Signin.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import img6  from '../images/Online Reading.png'
+import SignupButton from './SignupButton'
+import '../index.css'
+export default function Signin() {
+  return (
+    <div className='signin-layout'>
+    <div className='signin-left-display'>
+      <img className='signin-img' src={img6} alt="" />
+      <p className='signup-text'>Already a member? Sign in</p>
+    </div>
+    <div className='signin-right-display'>
+      <p className='signin-welcome'>Welcome to Lets Study</p>
+      <label className='signin-label'>Name</label>
+      <input className='signin-input' type="text" />
+      <label className='signin-label'>Email</label>
+      <input className='signin-input' type="email" />
+      <label className='signin-label'>Password</label>
+      <input className='signin-input' type="password" />
+      <label className='signin-label'>Date of Birth</label>
+      <input className='signin-input' type="date" />
+      <label className='signin-label'>Profile Picture</label>
+      <input className='signin-input' type="file" />
+      <SignupButton content={'Sign in'}></SignupButton>
+    </div>
+  </div>
+  );
+}
