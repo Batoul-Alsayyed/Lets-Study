916bce0b2e0ec3caeab7bcced62a904eaf3f3325
Batoul-Alsayyed <batoul.alsayyed20@gmail.com>
2022-07-10 15:19:18 +0300

    Frontend Adding card component

---
+++ frontend/src/components/Card.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import '../index.css'
+import student from '../images/asian-college-student-400x400 2.png'
+import { FaStar } from "react-icons/fa";
+import { MdLocationOn } from "react-icons/md";
+export default function Card() {
+  return (
+        <div className="card">
+            <img src={student} alt="" />
+            <div>
+                <p>Student name</p>
+                <span className='rate'>1.2</span>
+                <FaStar className='star-icon'></FaStar>
+                
+                </div>
+            <div><p>Beirut, Lebanon</p>
+            <span className='location-icon'><MdLocationOn/></span>
+            </div>
+            <div><p>23 years old</p></div>
+      </div>
+  )
+}
