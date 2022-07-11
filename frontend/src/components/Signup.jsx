44ee7bd733b093092d9743e89c62acc8eedbebcf
Batoul-Alsayyed <batoul.alsayyed20@gmail.com>
2022-07-11 17:37:23 +0300

    Frontend: Adding password Confirm + importing index.css + Fixing typo mistakes

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -1,21 +1,31 @@
 import React from 'react'
 import img6  from '../images/Online Reading.png'
 import SignupButton from './SignupButton'
-export default function Signup() {
+import '../index.css'
+export default function Signin() {
   return (
-    <div className='signup-layout'>
-      <div className='signup-left-display'>
-        <img className='signup-img' src={img6} alt="" />
-        <p className='signup-text'>Not a member? Register now</p>
-      </div>
-      <div className='signup-right-display'>
-        <p className='signup-welcome'>Welcome to Lets Study</p>
-        <label className='signup-label'>Email</label>
-        <input className='signup-input' type="text" />
-        <label className='signup-label'>Password</label>
-        <input className='signup-input' type="password" />
-        <SignupButton content={'Sign up'}></SignupButton>
-      </div>
+    <div className='signin-layout'>
+    <div className='signin-left-display'>
+      <img className='signin-img' src={img6} alt="" />
+      <p className='signup-text'>Already a member? Sign in</p>
     </div>
-  )
+    <div className='signin-right-display'>
+      <p className='signin-welcome'>Welcome to Lets Study</p>
+      <label className='signin-label'>Name</label>
+      <input className='signin-input' type="text" />
+      <label className='signin-label'>Email</label>
+      <input className='signin-input' type="email" />
+      <label className='signin-label'>Password</label>
+      <input className='signin-input' type="password" />
+      
+      <label className='signin-label'>Confirm Password</label>
+      <input className='signin-input' type="password" />
+      
+      <label className='signin-label'>Date of Birth</label>
+      <input className='signin-input' type="date" />
+     
+      <SignupButton content={'Sign in'}></SignupButton>
+    </div>
+  </div>
+  );
 }
