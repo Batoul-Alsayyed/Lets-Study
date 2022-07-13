1aed1b34b7b682d3dbdbf47b785f675e46561ad9
Batoul-Alsayyed <batoul.alsayyed20@gmail.com>
2022-07-13 19:20:16 +0300

    Frontend: Adding popup component

---
+++ frontend/src/components/Popup.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import '../index.css';
+import { AiOutlineClose } from "react-icons/ai";
+
+const Popup = props => {
+  return (
+    <div className="popup-box">
+      <div className="box">
+        <AiOutlineClose className="close-icon" onClick={props.handleClose}></AiOutlineClose>
+        {props.content}
+      </div>
+    </div>
+  );
+};
+ 
+export default Popup;
