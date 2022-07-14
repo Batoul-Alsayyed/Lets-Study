1f5706761e5c901473a9369a2962c152d56758e3
Batoul-Alsayyed <batoul.alsayyed20@gmail.com>
2022-07-14 16:55:15 +0300

    Backend: Adding students table

---
+++ Backend/database/migrations/2022_07_14_133320_create_students_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->string('account_type');
+            $table->decimal('image_link');
+            $table->decimal('rate_number');
+            $table->decimal('longitude');
+            $table->decimal('latitude');
+            $table->integer('degrees_id');
+            $table->integer('study_fields_id');
+            $table->integer('interested_field_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
