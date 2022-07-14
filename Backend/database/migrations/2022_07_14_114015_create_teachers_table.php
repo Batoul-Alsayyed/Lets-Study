c854f5ca63192619e6b087277a0e526015c1a719
Batoul-Alsayyed <batoul.alsayyed20@gmail.com>
2022-07-14 16:24:55 +0300

    Backend: Adding teachers table

---
+++ Backend/database/migrations/2022_07_14_114015_create_teachers_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTeachersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('teachers', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->string('image_link');
+            $table->decimal('rate_number');
+            $table->decimal('longitude');
+            $table->decimal('latitude');
+            $table->integer('degrees_id');
+            $table->integer('study_fields_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('teachers');
+    }
+}
