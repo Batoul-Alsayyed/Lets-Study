00d93a81205b98d4d9704af7e7a66c0e6f920e9c
Batoul-Alsayyed <batoul.alsayyed20@gmail.com>
2022-07-18 15:34:26 +0300

    Frontend: Making the code neater

--- frontend/src/components/Popup.jsx
+++ frontend/src/components/Popup.jsx
@@ -1,16 +1,19 @@
 import React from "react";
-import '../index.css';
+import "../index.css";
 import { AiOutlineClose } from "react-icons/ai";
 
-const Popup = props => {
+const Popup = (props) => {
   return (
     <div className="popup-box">
       <div className="box">
-        <AiOutlineClose className="close-icon" onClick={props.handleClose}></AiOutlineClose>
+        <AiOutlineClose
+          className="close-icon"
+          onClick={props.handleClose}
+        ></AiOutlineClose>
         {props.content}
       </div>
     </div>
   );
 };
- 
+
 export default Popup;
