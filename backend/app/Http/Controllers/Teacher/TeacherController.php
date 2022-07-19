fab18ae0e5f3fa3df29d524f0daf9e89a3dc3f09
Batoul-Alsayyed <batoul.alsayyed20@gmail.com>
2022-07-19 22:47:07 +0300

    Backend: Removing addTeacher from teacher controller

--- backend/app/Http/Controllers/Teacher/TeacherController.php
+++ backend/app/Http/Controllers/Teacher/TeacherController.php
@@ -15,32 +15,10 @@ class TeacherController extends Controller
      * @return void
      */
     public function __construct() {
-        $this->middleware('auth:api', ['except' => ['addTeacher']]);
+        $this->middleware('auth:api', ['except' => ['editProfile']]);
     }
-    //Adding new teacher (a teacher is a user but with extra attributes)
-    //Teachers will not be able to register their own accounts 
-    //only admin can add them through admin panel
+    //Teachers can edit their profile when they log in 
+    
 
-    public function addTeacher(Request $request) {
-        $validator = Validator::make($request->all(), [
-            'user_id'=>'required|integer',
-            'image_link'=>'required|string',
-            'rate_number'=>'required',
-            'longitude'=>'required',
-            'latitude'=>'required',
-            'degrees_id'=>'required|integer',
-            'study_fields_id'=>'required|integer',
 
-        ]);
-        if($validator->fails()){
-            return response()->json($validator->errors()->toJson(), 400);
-        }
-        $teacher = Teacher::create(array_merge(
-                    $validator->validated()
-                ));
-        return response()->json([
-            'message' => 'Teacher successfully added',
-            'teacher' => $teacher
-        ], 201);
-    }
 }
