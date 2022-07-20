5e04f7f40a33cfbd858c4b25192bfb38efce0d02
Batoul-Alsayyed <batoul.alsayyed20@gmail.com>
2022-07-20 18:26:45 +0300

    Frontend: Changing navigation links to anchor elements + adding navigation to them

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,24 +1,26 @@
 import React from "react";
 import "../index.css";
-import logo from '../images/logo/logo.png';
+import logo from "../images/logo/logo.png";
+import { useNavigate } from "react-router-dom";
+
 export default function Navbar() {
   return (
     <div className="navbar">
-        <div className="navbar-container">
-            <div className="left-side">
-                <div className="left-side-components">
-                    <img src={logo} alt="logo"/>
-                    <p>Lets Study</p>
-                </div>
-            </div>
-            <div className="right-side">
-                <div className="right-side-components">
-                    <p>Home</p>
-                    <p>Register</p>
-                    <p>Login</p>
-                </div>
-            </div>
+      <div className="navbar-container">
+        <div className="left-side">
+          <div className="left-side-components">
+            <img src={logo} alt="logo" />
+            <p>Lets Study</p>
+          </div>
         </div>
+        <div className="right-side">
+          <div className="right-side-components">
+            <a href="/home">Home</a>
+            <a href="/Signup">Register</a>
+            <a href="/Signin">Login</a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
