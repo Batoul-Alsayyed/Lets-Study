98f50b2530adb347b7706c4a4931eb18f3d1be02
Batoul-Alsayyed <batoul.alsayyed20@gmail.com>
2022-07-20 18:28:02 +0300

    Frontend: Removing white button element + adding navigation when clicking on the buttons

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,31 +1,49 @@
-import React from 'react'
-import Navbar from '../components/Navbar';
+import React from "react";
+import Navbar from "../components/Navbar";
 import "../index.css";
-import header from  '../images/header.png'
-import WhiteButton from './WhiteButton';
+import header from "../images/header.png";
+import { useNavigate } from "react-router-dom";
+
 export default function Header() {
+  let navigate = useNavigate();
+
+  const studyOnClick = (e) => {
+    e.preventDefault();
+    navigate("/Signup");
+  };
+
+  const proOnClick = (e) => {
+    e.preventDefault();
+    navigate("/Signup");
+  };
   return (
     <div>
-        <Navbar/>   
-        <div className="header-container">
+      <Navbar />
+      <div className="header-container">
         <div className="left-side">
-                <div className="header-left-side-components">
-                    <p className='header-p'>First Online Study Platform</p>
-                    <p className='start-p'>Start your study journey with students
-                        <br/> just like you or with a professional<br/> tutor.</p>
-                    <div className="buttons-styling">
-                    <button className='button1'>Join as a pro member</button>
-                    <WhiteButton content={"Study For Free"}></WhiteButton>
-                    </div>
-
-                </div>
+          <div className="header-left-side-components">
+            <p className="header-p">First Online Study Platform</p>
+            <p className="start-p">
+              Start your study journey with students
+              <br /> just like you or with a professional
+              <br /> tutor.
+            </p>
+            <div className="buttons-styling">
+              <button className="button1" onClick={proOnClick}>
+                Join as a pro member
+              </button>
+              <button className="button2" onClick={studyOnClick}>
+                Study For Free
+              </button>
             </div>
-            <div className="right-side">
-                <div className="header-right-side-components">
-                    <img src={header} />
-                </div>
-            </div>
+          </div>
         </div>
+        <div className="right-side">
+          <div className="header-right-side-components">
+            <img src={header} />
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
