a45881937a2be27ec9ecc7448bcbebcd521572c0
Batoul-Alsayyed <batoul.alsayyed20@gmail.com>
2022-07-20 20:42:44 +0300

    Backend: Adding degree model

---
+++ backend/app/Models/Degree.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+
+class Degree extends Model
+{
+    use HasFactory;
+      protected $fillable = [
+        'name'
+    ];
+    public function getJWTIdentifier() {
+        return $this->getKey();
+    }
+    public function getJWTCustomClaims() {
+        return [];
+    }    
+
+}
