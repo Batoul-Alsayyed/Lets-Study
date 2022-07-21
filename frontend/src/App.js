c3d245817613fd174eac09383cb70224cbcbe793
Batoul-Alsayyed <batoul.alsayyed20@gmail.com>
2022-07-21 13:26:37 +0300

    Frontend: Testing and calling newly added routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,41 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter as Router, Switch, Route, Link, BrowserRouter, Routes } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Header from './components/Header';
-import StudentsSection from './components/StudentsSection';
-import Homepage from './components/Homepage';
-import BookNow from './components/BookNow';
-import Footer from './components/Footer';
-import Signup from './components/Signin';
-import Signin from './components/Signup';
-import Students from './components/Students';
-import Student from './components/Student';
-import LoginNavbar from './components/LoginNavbar';
-import Popup from './components/Popup';
-import MapComponent from './components/MapComponent';
+import logo from "./logo.svg";
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  BrowserRouter,
+  Routes,
+} from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import StudentsSection from "./components/StudentsSection";
+import Homepage from "./components/Homepage";
+import BookNow from "./components/BookNow";
+import Footer from "./components/Footer";
+import Signup from "./components/Signin";
+import Signin from "./components/Signup";
+import Students from "./components/Students";
+import Student from "./components/Student";
+import LoginNavbar from "./components/LoginNavbar";
+import Popup from "./components/Popup";
+import MapComponent from "./components/MapComponent";
+import TeacherProfile from "./components/TeacherProfile";
+import StudentProfile from "./components/StudentProfile";
 function App() {
   return (
-      <div>
-        <BrowserRouter>
+    <div>
+      <BrowserRouter>
         <Routes>
-          <Route path='/navbar' element={<Navbar/>}></Route>
-          <Route path='/header' element={<Header/>}></Route>
-          <Route path='/students-section' element={<StudentsSection/>}></Route>
-          <Route path='/home' element={<Homepage/>}></Route>
-          <Route path='/book' element={<BookNow/>}></Route>
-          <Route path='/footer' element={<Footer/>}></Route>
-          <Route path='/Signin' element={<Signup/>}></Route>
-          <Route path='/Signup' element={<Signin/>}></Route>
-          <Route path='/students' element={<Students/>}></Route>
-          <Route path='/student' element={<Student/>}></Route>
-          <Route path='/login-navbar' element={<LoginNavbar/>}></Route>
-          <Route path='/popup' element={<Popup/>}></Route>
-          <Route path='/map' element={<MapComponent/>}></Route>
+          <Route path="/navbar" element={<Navbar />}></Route>
+          <Route path="/header" element={<Header />}></Route>
+          <Route path="/students-section" element={<StudentsSection />}></Route>
+          <Route path="/home" element={<Homepage />}></Route>
+          <Route path="/book" element={<BookNow />}></Route>
+          <Route path="/footer" element={<Footer />}></Route>
+          <Route path="/Signin" element={<Signup />}></Route>
+          <Route path="/Signup" element={<Signin />}></Route>
+          <Route path="/students" element={<Students />}></Route>
+          <Route path="/student" element={<Student />}></Route>
+          <Route path="/login-navbar" element={<LoginNavbar />}></Route>
+          <Route path="/popup" element={<Popup />}></Route>
+          <Route path="/map" element={<MapComponent />}></Route>
+          <Route path="/teacher-profile" element={<TeacherProfile />}></Route>
+          <Route path="/student/:id" element={<StudentProfile />}></Route>
         </Routes>
-        </BrowserRouter>
-      </div>
-  )
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
