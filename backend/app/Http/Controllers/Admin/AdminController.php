7903df2bd93506df26a622bcc285790810d80780
Batoul-Alsayyed <batoul.alsayyed20@gmail.com>
2022-07-21 13:20:59 +0300

    Backend: Adding addDegree and addStudyField APIs

--- backend/app/Http/Controllers/Admin/AdminController.php
+++ backend/app/Http/Controllers/Admin/AdminController.php
@@ -5,19 +5,18 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
 use App\Models\Teacher;
+use App\Models\StudyField;
+use App\Models\Degree;
 use App\Http\Controllers\Controller;
 use Validator;
 
 class AdminController extends Controller{
-    public function __construct() {
-        $this->middleware('auth:api', ['except' => ['addDegree, addStudyField','updateUserType','addTeacher']]);
-    }
-
     //Adding new teacher (a teacher is a user but with extra attributes)
     //Teachers will not be able to register their own accounts 
     //only admin can add them through admin panel
 
     public function addTeacher(Request $request) {
+        
         $validator = Validator::make($request->all(), [
             'user_id'=>'required|integer',
             'image_link'=>'required|string',
@@ -37,6 +36,40 @@ class AdminController extends Controller{
         return response()->json([
             'message' => 'Teacher successfully added',
             'teacher' => $teacher
+        ], 201);
+    }
+        public function addDegree(Request $request) {
+
+        $validator = Validator::make($request->all(), [
+            'name'=>'required|string',
+
+        ]);
+        if($validator->fails()){
+            return response()->json($validator->errors()->toJson(), 400);
+        }
+        $degree = Degree::create(array_merge(
+                    $validator->validated()
+                ));
+        return response()->json([
+            'message' => 'Degree successfully added',
+            'degree' => $degree
+        ], 201);
+    }
+        public function addStudyField(Request $request) {
+
+        $validator = Validator::make($request->all(), [
+            'name'=>'required|string',
+
+        ]);
+        if($validator->fails()){
+            return response()->json($validator->errors()->toJson(), 400);
+        }
+        $study_field = StudyField::create(array_merge(
+                    $validator->validated()
+                ));
+        return response()->json([
+            'message' => 'Studyfield successfully added',
+            'Study_field' => $study_field
         ], 201);
     }
 }
