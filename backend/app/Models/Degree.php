968efe691d561cd728f4b43130de2f6b312a5af2
Batoul-Alsayyed <batoul.alsayyed20@gmail.com>
2022-07-21 13:23:32 +0300

    Backend: Solving bug in degree model

--- backend/app/Models/Degree.php
+++ backend/app/Models/Degree.php
@@ -1,11 +1,9 @@
 <?php
 
 namespace App\Models;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Database\Eloquent\Model;
 
 class Degree extends Model
 {
@@ -13,11 +11,5 @@ class Degree extends Model
       protected $fillable = [
         'name'
     ];
-    public function getJWTIdentifier() {
-        return $this->getKey();
-    }
-    public function getJWTCustomClaims() {
-        return [];
-    }    
-
+    
 }
