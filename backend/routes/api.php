ca8b693d162e16dce145def2ef33c37fc6044c3f
Batoul-Alsayyed <batoul.alsayyed20@gmail.com>
2022-07-21 13:27:23 +0300

    Backend: Testing newly added APIs

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\User\UserController;
 use App\Http\Controllers\Teacher\TeacherController;
 use App\Http\Controllers\Student\StudentController;
+use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\RatingController;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,16 +26,26 @@ Route::group([
     Route::post('/refresh', [UserController::class, 'refresh']);
     Route::get('/user-profile', [UserController::class, 'userProfile']);    
     Route::post('/addUserType', [UserController::class, 'addUserType']);    
+    Route::post('/getUserById', [UserController::class, 'getUserById']);    
+    Route::post('/add_rating', [RatingController::class, 'addRating']); 
+    Route::post('/updateRateNumberByUserId', [RatingController::class, 'updateRateNumberByUserId']); 
+    
+
 });
 Route::group([
     'middleware' => 'api',
-    'prefix' => 'teacher'
+    'prefix' => 'admin'
 ], function ($router) {
-    Route::post('/add_teacher', [TeacherController::class, 'addTeacher']);  
+    Route::post('/add_teacher', [AdminController::class, 'addTeacher']);  
+    Route::post('/add_studyfield', [AdminController::class, 'addStudyField']);  
+    Route::post('/add_degree', [AdminController::class, 'addDegree']);  
 });
 Route::group([
     'middleware' => 'api',
     'prefix' => 'student'
 ], function ($router) {
-    Route::post('/add_student', [StudentController::class, 'addStudent']);  
+    Route::post('/add_student', [StudentController::class, 'addStudent']); 
+    Route::post('/getStudentById', [StudentController::class, 'getStudentById']);  
+    Route::get('/students', [StudentController::class, 'getAllStudents']);  
 });
+
