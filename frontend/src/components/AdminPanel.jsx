b899128e38b0ea6b795e2b8cf8b3453cf25d40bf
Batoul-Alsayyed <batoul.alsayyed20@gmail.com>
2022-07-23 02:37:05 +0300

    Frontend: Adding Adminpanel component and calling the newly created navbar inside it

---
+++ frontend/src/components/AdminPanel.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../index.css";
+import AdminNavbar from "../components/AdminNavbar";
+
+function AdminPanel() {
+  return (
+    <div className="adminpanel-container">
+      <AdminNavbar />
+      <p className="text"></p>
+      <div></div>
+    </div>
+  );
+}
+export default AdminPanel;
