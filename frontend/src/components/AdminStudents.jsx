445359a418001c4740f434d6878e4c32644315e7
Batoul-Alsayyed <batoul.alsayyed20@gmail.com>
2022-07-23 15:31:10 +0300

    Frontend: Adding AdminStudents component

---
+++ frontend/src/components/AdminStudents.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import AdminNavbar from "./AdminNavbar";
+export default function AdminStudents() {
+  return (
+    <div>
+      <AdminNavbar />
+      Admin students
+    </div>
+  );
+}
