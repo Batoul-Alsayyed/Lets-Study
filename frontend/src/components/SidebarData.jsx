e0c05e9f31f349940b00f5367bf6f00d3b0104fd
Batoul-Alsayyed <batoul.alsayyed20@gmail.com>
2022-07-23 15:32:52 +0300

    Frontend: Adding path to nav links + Removing unnecessary libraries

--- frontend/src/components/SidebarData.jsx
+++ frontend/src/components/SidebarData.jsx
@@ -1,22 +1,23 @@
 import React from "react";
-import * as FaIcons from "react-icons/fa";
-import * as AiIcons from "react-icons/ai";
 import * as FiIcons from "react-icons/fi";
 import Student from "../images/Vector.png";
 import Teacher from "../images/Teaching.png";
 export const SidebarData = [
   {
     title: "Students",
+    path: "/admin-students",
     icon: <img src={Student} alt="" className="admin-icons" />,
     cName: "nav-text",
   },
   {
     title: "Teachers",
+    path: "/admin-teachers",
     icon: <img src={Teacher} alt="" className="admin-icons" />,
     cName: "nav-text",
   },
   {
     title: "Logout",
+    path: "/home",
     icon: <FiIcons.FiLogOut className="admin-icons" size={25} />,
     cName: "nav-text",
   },
