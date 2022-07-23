b10408a351e0cbf5c3db3e02880e2a363325dc6b
Batoul-Alsayyed <batoul.alsayyed20@gmail.com>
2022-07-23 17:59:25 +0300

    Frontend : Adding teachers table

--- frontend/src/components/AdminTeachers.jsx
+++ frontend/src/components/AdminTeachers.jsx
@@ -1,10 +1,36 @@
 import React from "react";
+import "../index.css";
 import AdminNavbar from "./AdminNavbar";
+
 export default function AdminTeachers() {
   return (
-    <div>
+    <div className="adminpanel-container">
       <AdminNavbar />
-      Admin teachers
+      <div className="teacher-text">
+        <h1>Teachers</h1>
+      </div>
+      <div className="admin-table">
+        <table>
+          <tr>
+            <th>id</th>
+            <th>image_link</th>
+            <th>rate_number</th>
+            <th>longitude</th>
+            <th>latitude</th>
+            <th>degrees_id</th>
+            <th>study_fields_id</th>
+          </tr>
+          <tr>
+            <td>1</td>
+            <td>link</td>
+            <td>rate</td>
+            <td>rate</td>
+            <td>rate</td>
+            <td>rate</td>
+            <td>rate</td>
+          </tr>
+        </table>
+      </div>
     </div>
   );
 }
