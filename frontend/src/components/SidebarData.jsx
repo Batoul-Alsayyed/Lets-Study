b7d90b850e582b219f79050f92daef1127f95c7f
Batoul-Alsayyed <batoul.alsayyed20@gmail.com>
2022-07-25 00:31:32 +0300

    Frontend: Adding degrees to navbar

--- frontend/src/components/SidebarData.jsx
+++ frontend/src/components/SidebarData.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import * as FiIcons from "react-icons/fi";
 import * as AiIcons from "react-icons/ai";
+import * as RiIcons from "react-icons/ri";
 import Student from "../images/Vector.png";
 import Teacher from "../images/Teaching.png";
 export const SidebarData = [
@@ -20,6 +21,12 @@ export const SidebarData = [
     title: "Teachers",
     path: "/admin-teachers",
     icon: <img src={Teacher} alt="" className="admin-icons" />,
+    cName: "nav-text",
+  },
+  {
+    title: "Degrees",
+    path: "/admin-degrees",
+    icon: <RiIcons.RiBookMarkLine size={25} />,
     cName: "nav-text",
   },
   {
