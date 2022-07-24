655d90c258de0f82877dbbd98fd7bc06761543cd
Batoul-Alsayyed <batoul.alsayyed20@gmail.com>
2022-07-25 01:10:42 +0300

    Frontend: Adding add degree + delete degree forms + linking add degree with its API

--- frontend/src/components/AdminDegrees.jsx
+++ frontend/src/components/AdminDegrees.jsx
@@ -1,10 +1,48 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "../index.css";
+import StudentPopup from "./StudentPopup";
 import AdminNavbar from "./AdminNavbar";
 export default function AdminDegrees() {
   const [data, setData] = useState(null);
+  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen2, setIsOpen2] = useState(false);
+  const [degree_id, setDegreeID] = useState(null);
+  const [degree_name, setDegreeName] = useState(null);
 
+  const togglePopup = () => {
+    setIsOpen(!isOpen);
+  };
+  const togglePopup2 = () => {
+    setIsOpen2(!isOpen2);
+  };
+  const HandleAddDegree = (e) => {
+    e.preventDefault();
+    togglePopup();
+  };
+  const HandleDeleteDegree = (e) => {
+    e.preventDefault();
+    togglePopup2();
+  };
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log(degree_name);
+    axios
+      .post(`http://127.0.0.1:8000/api/admin/add_degree`, {
+        name: degree_name,
+      })
+      .then((res) => {
+        console.log(res);
+        togglePopup();
+      })
+      .catch((err) => {
+        togglePopup();
+        console.log(err);
+      });
+  };
+  const submitHandler2 = (e) => {
+    e.preventDefault();
+  };
   useEffect(() => {
     axios
       .get(`http://127.0.0.1:8000/api/admin/degrees`)
@@ -37,7 +75,6 @@ export default function AdminDegrees() {
           </tr>
           {data &&
             data.map((i, index) => {
-              console.log(i);
               return (
                 <tr>
                   <td>{i.id}</td>
@@ -46,7 +83,85 @@ export default function AdminDegrees() {
               );
             })}
         </table>
+        <div className="admin-buttons">
+          <button className="admin-btn" onClick={HandleAddDegree}>
+            Add
+          </button>
+          <button className="admin-btn" onClick={HandleDeleteDegree}>
+            Delete
+          </button>
+        </div>
       </div>
+      {isOpen && (
+        <StudentPopup
+          className="popup-size"
+          content={
+            <>
+              <form className="teacher-form" onSubmit={submitHandler}>
+                <div className="teacher-form-item">
+                  <h1>Add a Degree</h1>
+                  <br />
+                  <br />
+
+                  <div className="teacher-div">
+                    <label htmlFor="" className="teacher-div-label">
+                      Degree Name:{" "}
+                    </label>
+                    <br />
+                    <input
+                      type="text"
+                      className="teacher-div-input"
+                      onChange={(e) => setDegreeName(e.target.value)}
+                    />
+                  </div>
+                  <button
+                    className="teacher-form-item-button delete"
+                    type="submit"
+                  >
+                    Submit
+                  </button>
+                </div>
+              </form>
+            </>
+          }
+          handleClose={togglePopup}
+        />
+      )}
+      {isOpen2 && (
+        <StudentPopup
+          className="popup-size"
+          content={
+            <>
+              <form className="teacher-form" onSubmit={submitHandler2}>
+                <div className="teacher-form-item">
+                  <h1>Delete a Degree</h1>
+                  <br />
+                  <br />
+
+                  <div className="teacher-div">
+                    <label htmlFor="" className="teacher-div-label">
+                      Degree ID:{" "}
+                    </label>
+                    <br />
+                    <input
+                      type="text"
+                      className="teacher-div-input"
+                      onChange={(e) => setDegreeID(e.target.value)}
+                    />
+                  </div>
+                  <button
+                    className="teacher-form-item-button delete"
+                    type="submit"
+                  >
+                    Submit
+                  </button>
+                </div>
+              </form>
+            </>
+          }
+          handleClose={togglePopup2}
+        />
+      )}
     </div>
   );
 }
