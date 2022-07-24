86032b0338c143b6bd2b35b652db357e4da52c83
Batoul-Alsayyed <batoul.alsayyed20@gmail.com>
2022-07-24 15:35:35 +0300

    Frontend: Adding add teacher popup form

--- frontend/src/components/AdminTeachers.jsx
+++ frontend/src/components/AdminTeachers.jsx
@@ -1,10 +1,16 @@
 import React, { useEffect, useState } from "react";
+import StudentPopup from "./StudentPopup";
 import "../index.css";
 import AdminNavbar from "./AdminNavbar";
 import axios from "axios";
+import ReactStars from "react-rating-stars-component";
 
 export default function AdminTeachers() {
   const [data, setData] = useState(null);
+  const [isOpen, setIsOpen] = useState(false);
+  const togglePopup = () => {
+    setIsOpen(!isOpen);
+  };
 
   useEffect(() => {
     axios
@@ -23,7 +29,11 @@ export default function AdminTeachers() {
         console.log(err);
       });
   }, []);
-
+  const HandleAddTeacher = (e) => {
+    e.preventDefault();
+    console.log("Clicked");
+    togglePopup();
+  };
   return (
     <div className="adminpanel-container">
       <AdminNavbar />
@@ -60,10 +70,120 @@ export default function AdminTeachers() {
             })}
         </table>
         <div className="admin-buttons">
-          <button className="admin-btn">Add</button>
+          <button className="admin-btn" onClick={HandleAddTeacher}>
+            Add
+          </button>
           <button className="admin-btn">Delete</button>
         </div>
       </div>
+      {isOpen && (
+        <StudentPopup
+          className="popup-size"
+          content={
+            <>
+              <form action="" className="teacher-form">
+                <div className="teacher-form-item">
+                  <h1>Add a Teacher</h1>
+                  <br />
+                  <br />
+
+                  <div className="teacher-div">
+                    <label htmlFor="" className="teacher-div-label">
+                      Full Name:{" "}
+                    </label>
+                    <br />
+                    <input type="text" className="teacher-div-input" />
+                  </div>
+
+                  <div className="teacher-div">
+                    <label htmlFor="" className="teacher-div-label">
+                      Email:{" "}
+                    </label>
+                    <br />
+
+                    <input type="email" className="teacher-div-input" />
+                  </div>
+
+                  <div className="teacher-div">
+                    <label htmlFor="" className="teacher-div-label">
+                      Password:{" "}
+                    </label>
+                    <br />
+
+                    <input type="password" className="teacher-div-input" />
+                  </div>
+
+                  <div className="teacher-div">
+                    <label htmlFor="" className="teacher-div-label">
+                      Date of Birth:{" "}
+                    </label>
+                    <br />
+
+                    <input type="date" className="teacher-div-input" />
+                  </div>
+
+                  <div className="teacher-div">
+                    <label htmlFor="" className="teacher-div-label">
+                      Longitude:{" "}
+                    </label>
+                    <br />
+
+                    <input type="text" className="teacher-div-input" />
+                  </div>
+                  <div className="teacher-div">
+                    <label htmlFor="" className="teacher-div-label">
+                      Latitude:{" "}
+                    </label>
+                    <br />
+
+                    <input type="text" className="teacher-div-input" />
+                  </div>
+                  <div className="teacher-div">
+                    <label htmlFor="" className="teacher-div-label">
+                      latitude:{" "}
+                    </label>
+                    <br />
+
+                    <input type="text" className="teacher-div-input" />
+                  </div>
+
+                  <div className="teacher-div">
+                    <label htmlFor="" className="teacher-div-label">
+                      Degrees:{" "}
+                    </label>
+                    <br />
+
+                    <select name="" id="" className="teacher-div-input select">
+                      <option value="">Bachelor</option>
+                      <option value="">Masters</option>
+                    </select>
+                  </div>
+
+                  <div className="teacher-div">
+                    <label htmlFor="" className="teacher-div-label">
+                      Study field:{" "}
+                    </label>
+                    <br />
+                    <select name="" id="" className="teacher-div-input select">
+                      <option value="">computer Science</option>
+                      <option value="">Biology</option>
+                    </select>
+                  </div>
+                  <div className="teacher-div">
+                    <label htmlFor="" className="teacher-div-label">
+                      Image Link:{" "}
+                    </label>
+                    <br />
+                    <input type="file" className="teacher-div-input" />
+                  </div>
+                  <button className="teacher-form-item-button">Submit</button>
+                </div>
+              </form>
+            </>
+          }
+          handleClose={togglePopup}
+        />
+      )}
     </div>
   );
 }
