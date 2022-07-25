38c2dd5850021d69b34191a7269d5ae36b93609b
Batoul-Alsayyed <batoul.alsayyed20@gmail.com>
2022-07-25 14:56:42 +0300

    Frontend: Adding classes to card elements

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,22 +1,26 @@
-import React from 'react'
-import '../index.css'
-import student from '../images/asian-college-student-400x400 2.png'
+import React from "react";
+import "../index.css";
+import student from "../images/asian-college-student-400x400 2.png";
 import { FaStar } from "react-icons/fa";
 import { MdLocationOn } from "react-icons/md";
 export default function Card() {
   return (
-        <div className="card">
-            <img src={student} alt="" />
-            <div>
-                <p>Student name</p>
-                <span className='rate'>1.2</span>
-                <FaStar className='star-icon'></FaStar>
-                
-                </div>
-            <div><p>Beirut, Lebanon</p>
-            <span className='location-icon'><MdLocationOn/></span>
-            </div>
-            <div><p>23 years old</p></div>
+    <div className="card">
+      <img src={student} alt="" className="card-img" />
+      <div>
+        <p>Student name</p>
+        <span className="rate">1.2</span>
+        <FaStar className="star-icon"></FaStar>
       </div>
-  )
+      <div className="location-div">
+        <p>Beirut, Lebanon</p>
+        <span className="location-icon">
+          <MdLocationOn />
+        </span>
+      </div>
+      <div className="age-div">
+        <p>23 years old</p>
+      </div>
+    </div>
+  );
 }
