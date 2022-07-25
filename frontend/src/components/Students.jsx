1ff7e1507ea2cbf2f011ec4ea99a3e2f1b4dd5d5
Batoul-Alsayyed <batoul.alsayyed20@gmail.com>
2022-07-25 23:58:34 +0300

    Frontend: Linking students page with its API

--- frontend/src/components/Students.jsx
+++ frontend/src/components/Students.jsx
@@ -1,14 +1,29 @@
-import React from 'react'
-import LoginNavbar from './LoginNavbar'
-import SimpleSlider from './SimpleSlider';
-import Card from '../components/Card';
+import React, { useState, useEffect } from "react";
+import LoginNavbar from "./LoginNavbar";
+import SimpleSlider from "./SimpleSlider";
+import axios from "axios";
+
 export default function Students() {
+  const [students, setStudents] = useState(null);
+  async function getStudents() {
+    await axios
+      .get(`http://127.0.0.1:8000/api/admin/students`)
+      .then((res) => {
+        setStudents(res.data.students);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+  useEffect(() => {
+    getStudents();
+  }, []);
+
   return (
-    <div className='students-page'>
-        <LoginNavbar/>
-      <p className='students-text'>Choose a student to study with: </p>
-      <SimpleSlider/>      
+    <div className="students-page">
+      <LoginNavbar />
+      <p className="students-text">Choose a student to study with: </p>
+      {students && <SimpleSlider studentsarray={students} />}
     </div>
-    
-  )
+  );
 }
