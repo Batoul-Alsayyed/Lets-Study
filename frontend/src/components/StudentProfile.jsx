729cad094ee3af97c49aaef174a50e1d43b63490
Batoul-Alsayyed <batoul.alsayyed20@gmail.com>
2022-07-26 12:08:41 +0300

    Frontend: Linking student profile with its API

--- frontend/src/components/StudentProfile.jsx
+++ frontend/src/components/StudentProfile.jsx
@@ -6,14 +6,27 @@ import LoginNavbar from "../components/LoginNavbar";
 import profile from "../images/Frame 4.png";
 import { RiStarFill } from "react-icons/ri";
 import { BsChatQuote } from "react-icons/bs";
+import axios from "axios";
 
 export default function StudentProfile() {
   const ratingChanged = (newRating) => {
     console.log(newRating);
   };
   const [isOpen, setIsOpen] = useState(false);
+  const [user, setUser] = useState({
+    name: "",
+    age: 0,
+    date_of_birth: "",
+    rate_number: "",
+    email: "",
+  });
+  const [info, setInfo] = useState({
+    image_link: "",
+    rate_number: "",
+  });
 
   let { id } = useParams();
+  console.log({ id }.id);
   const togglePopup = () => {
     setIsOpen(!isOpen);
   };
@@ -25,6 +38,71 @@ export default function StudentProfile() {
     e.preventDefault();
     togglePopup();
   };
+  function getName() {
+    console.log();
+
+    axios
+      .post(`http://127.0.0.1:8000/api/user/getUserById`, {
+        id: { id }.id,
+      })
+      .then((res) => {
+        console.log(res.data.user);
+        setUser({
+          ...user,
+          name: res.data.user[0].name,
+          date_of_birth: res.data.user[0].date_of_birth,
+          email: res.data.user[0].email,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+  function getInfo() {
+    console.log();
+
+    axios
+      .post(`http://127.0.0.1:8000/api/student/getStudentById`, {
+        user_id: { id }.id,
+      })
+      .then((res) => {
+        console.log("student=>", res.data.student[0].image_link);
+        setInfo({
+          ...info,
+          image_link: res.data.student[0].image_link,
+          rate_number: parseInt(res.data.student[0].rate_number),
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+  function getAge() {
+    if (user.date_of_birth) {
+      console.log(user.date_of_birth);
+      var currentDate = new Date();
+      console.log(currentDate);
+      var currentYear = currentDate.getFullYear();
+      console.log(currentYear);
+      const year_born = user.date_of_birth.split("-")[0];
+      console.log("year born ", year_born);
+      var age = currentYear - year_born;
+      console.log("age: ", age);
+      setUser({ ...user, age: age });
+    }
+  }
+  useEffect(() => {
+    getName();
+  }, []);
+
+  useEffect(() => {
+    getAge();
+    getInfo();
+  }, [user.date_of_birth]);
+
+  // useEffect(() => {
+  //   getInfo();
+  // }, []);
 
   return (
     <div>
@@ -34,7 +112,7 @@ export default function StudentProfile() {
         <RatePopup
           content={
             <div className="pop-up">
-              <h1 className="rate-text">Rate this student</h1>
+              <h1 className="rate-text">Rate {user.name}</h1>
               <ReactStars
                 count={5}
                 onChange={ratingChanged}
@@ -52,27 +130,30 @@ export default function StudentProfile() {
         />
       )}
 
-      <p className="student-profile-title">Student profile</p>
+      <p className="student-profile-title">{user.name} profile</p>
       <div className="student-profile">
         <div className="student-profile-content">
-          <div>Name: Student A</div>
-          <div>Age: 18 years old</div>
+          <div>
+            Name: <span className="user-email">{user.name}</span>
+          </div>
+          <div>
+            Age: <span className="user-email">{user.age} years old</span>
+          </div>
           <div>
             Rating:
             <span className="rate-stars" id="icons">
-              <RiStarFill className="star" />
-              <RiStarFill className="star" />
-              <RiStarFill className="star" />
-              <RiStarFill className="star" />
+              {[...Array(info.rate_number)].map((elementInArray, index) => {
+                return <RiStarFill className="star" />;
+              })}
             </span>
           </div>
-          <div>Location: Beirut, Lebanon</div>
           <div>
-            Email: Student@gmail.com <BsChatQuote />
+            Email: <span className="user-email">{user.email}</span>{" "}
+            <BsChatQuote />
           </div>
         </div>
         <div className="student-profile-content">
-          <img src={profile} className="student-profile-img" />
+          <img src={info.image_link} className="student-profile-img" />
         </div>
       </div>
       <div className="centered">
@@ -82,4 +163,7 @@ export default function StudentProfile() {
       </div>
     </div>
   );
+}
+{
+  /* <div>Location: Beirut, Lebanon</div> */
 }
