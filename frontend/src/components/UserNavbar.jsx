6ea8ba408b98f8aadff535f40ec7ca4a4181a210
Batoul-Alsayyed <batoul.alsayyed20@gmail.com>
2022-07-26 19:00:32 +0300

    Frontend: Adding user navbar component for logged in users and not registered students yet

---
+++ frontend/src/components/UserNavbar.jsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from "react";
+import "../index.css";
+import logo from "../images/logo/logo.png";
+import account from "../images/Account.png";
+import axios from "axios";
+
+export default function USerNavbar() {
+  const [path, setPath] = useState(0);
+  const [name, setName] = useState(0);
+
+  useEffect(() => {
+    //since we have the access token in local storage
+    //lets use it to retreive user info
+    // console.log(localStorage.getItem("access_token"));
+    let access_token = localStorage.getItem("access_token");
+    axios
+      .get(`http://127.0.0.1:8000/api/user/user-profile`, {
+        headers: { Authorization: `Bearer ${access_token}` },
+      })
+      .then((res) => {
+        setName(res.data.name);
+        setPath("/students/" + res.data.id);
+      });
+  });
+  return (
+    <div className="navbar">
+      <div className="navbar-container">
+        <div className="left-side">
+          <div className="left-side-components">
+            <img src={logo} alt="logo" />
+            <p>Lets Study</p>
+          </div>
+        </div>
+        <div className="right-side">
+          <div className="right-side-components">
+            <a href={path}>Study Now</a>
+            <p>Welcome {name}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
