0ed210830a4133aa97e0ddfcc9af7c3f9b0d271a
Batoul-Alsayyed <batoul.alsayyed20@gmail.com>
2022-08-05 15:09:33 +0300

    Frontend: Updating navbar for logged in user in homepage

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import LoginNavbar from "./LoginNavbar";
 import "../index.css";
 import header from "../images/header.png";
 import { useNavigate } from "react-router-dom";
 
 export default function Header() {
   let navigate = useNavigate();
-
+  let user_id = localStorage.getItem("user_id");
   const studyOnClick = (e) => {
     e.preventDefault();
     navigate("/Signup");
@@ -16,6 +17,39 @@ export default function Header() {
     e.preventDefault();
     navigate("/Signup");
   };
+  {
+    if (user_id) {
+      return (
+        <div>
+          <LoginNavbar />
+          <div className="header-container">
+            <div className="left-side">
+              <div className="header-left-side-components">
+                <p className="header-p">First Online Study Platform</p>
+                <p className="start-p">
+                  Start your study journey with students just like you or with a
+                  professional tutor.
+                </p>
+                <div className="buttons-styling">
+                  <button className="button1" onClick={proOnClick}>
+                    Join as a pro member
+                  </button>
+                  <button className="button2" onClick={studyOnClick}>
+                    Study For Free
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div className="right-side">
+              <div className="header-right-side-components">
+                <img src={header} />
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    }
+  }
   return (
     <div>
       <Navbar />
