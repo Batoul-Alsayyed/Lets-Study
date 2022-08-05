95259acd9de82c93e037caf7c988dea2d2a1a3c3
Batoul-Alsayyed <batoul.alsayyed20@gmail.com>
2022-08-05 22:04:42 +0300

    Frontend: Cleaning up the code

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -22,37 +22,25 @@ export default function Card({
   const OnClickHandler = () => {
     navigate("/student/" + student_user_id);
   };
-  // const [user, setUser] = useState(null);
   function getName() {
-    console.log(student_user_id);
-
     axios
       .post(`http://127.0.0.1:8000/api/user/getUserById`, {
         id: student_user_id,
       })
       .then((res) => {
-        console.log(res.data.user);
         setUser({
           ...user,
           name: res.data.user[0].name,
           date_of_birth: res.data.user[0].date_of_birth,
         });
-      })
-      .catch((err) => {
-        console.log(err);
       });
   }
   function getAge() {
     if (user.date_of_birth) {
-      console.log(user.date_of_birth);
       var currentDate = new Date();
-      console.log(currentDate);
       var currentYear = currentDate.getFullYear();
-      console.log(currentYear);
       const year_born = user.date_of_birth.split("-")[0];
-      console.log("year born ", year_born);
       var age = currentYear - year_born;
-      console.log("age: ", age);
       setUser({ ...user, age: age });
     }
   }
@@ -72,15 +60,6 @@ export default function Card({
         <span className="rate">{student_rate_number}</span>
         <FaStar className="star-icon"></FaStar>
       </div>
-      {/* <div className="location-div">
-        <p>
-          Latitude: {student_latitude}
-          Longitude: {student_longitude}
-        </p>
-        <span className="location-icon">
-          <MdLocationOn />
-        </span>
-      </div> */}
       <div className="age-div">
         <p>{user.age} years old</p>
       </div>
