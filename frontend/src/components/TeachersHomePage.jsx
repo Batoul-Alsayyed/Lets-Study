b225403bb3b4ed58bdf82588b96f5c0c664e01c2
Batoul-Alsayyed <batoul.alsayyed20@gmail.com>
2022-08-05 17:22:40 +0300

    Frontend: Adding teachers home page

---
+++ frontend/src/components/TeachersHomePage.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import LoginNavbar from "./LoginNavbar";
+import Teachers from "../images/brooke-cagle-g1Kr4Ozfoac-unsplash.jpg";
+import Footer from "./Footer";
+import "../index.css";
+export default function TeachersHomePage() {
+  return (
+    <div>
+      <div className="teachers-page">
+        <LoginNavbar />
+        <div className="teachers-header">
+          <div className="t-h">
+            <p className="teachers-header-p">Welcome to Lets Study</p>
+            <p className="teachers-header-p1">
+              Start your teaching journey with students from all around the
+              world.
+            </p>
+            <button className="t-h-button">View Notes</button>
+          </div>
+        </div>
+
+        <div className="teachers-header-left-side"></div>
+      </div>
+      <Footer />
+    </div>
+  );
+}
