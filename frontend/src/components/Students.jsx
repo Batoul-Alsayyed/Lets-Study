b884f8ed545ceedb633be4bfffd4cc5482c1dba1
Batoul-Alsayyed <batoul.alsayyed20@gmail.com>
2022-08-05 18:32:59 +0300

    Frontend: Filtering students and teachers so that the user won't be shown there

--- frontend/src/components/Students.jsx
+++ frontend/src/components/Students.jsx
@@ -5,15 +5,18 @@ import axios from "axios";
 
 export default function Students() {
   const [students, setStudents] = useState(null);
+  let user_id = localStorage.getItem("user_id");
+  const filtered_students = [];
   async function getStudents() {
-    await axios
-      .get(`http://127.0.0.1:8000/api/admin/students`)
-      .then((res) => {
-        setStudents(res.data.students);
-      })
-      .catch((err) => {
-        console.log(err);
+    await axios.get(`http://127.0.0.1:8000/api/admin/students`).then((res) => {
+      res.data.students.map((i, index) => {
+        if (String(i.user_id) !== String(user_id)) {
+          console.log("user id here ", i.user_id);
+          filtered_students.push(i);
+        }
       });
+      setStudents(filtered_students);
+    });
   }
   useEffect(() => {
     getStudents();
--- frontend/src/components/Teachers.jsx
+++ frontend/src/components/Teachers.jsx
@@ -5,15 +5,19 @@ import axios from "axios";
 
 export default function Students() {
   const [teachers, setTeachers] = useState(null);
+  let user_id = localStorage.getItem("user_id");
+  const filtered_teachers = [];
   async function getTeachers() {
-    await axios
-      .get(`http://127.0.0.1:8000/api/admin/teachers`)
-      .then((res) => {
-        setTeachers(res.data.teachers);
-      })
-      .catch((err) => {
-        console.log(err);
+    await axios.get(`http://127.0.0.1:8000/api/admin/teachers`).then((res) => {
+      res.data.teachers.map((i, index) => {
+        if (String(i.user_id) !== String(user_id)) {
+          console.log("user id here ", i.user_id);
+          filtered_teachers.push(i);
+        }
       });
+      console.log("Filtered teachers=>", filtered_teachers);
+      setTeachers(filtered_teachers);
+    });
   }
   useEffect(() => {
     getTeachers();
