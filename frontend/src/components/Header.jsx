a44d9d2fa91cea1013c64fc9a7eca7dcbef8f313
Batoul-Alsayyed <batoul.alsayyed20@gmail.com>
2022-08-05 15:46:11 +0300

    Frontend: Fixing proOnClick directing for logged in students

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,20 +2,52 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import LoginNavbar from "./LoginNavbar";
 import "../index.css";
+import axios from "axios";
 import header from "../images/header.png";
 import { useNavigate } from "react-router-dom";
 
 export default function Header() {
   let navigate = useNavigate();
   let user_id = localStorage.getItem("user_id");
+  let user_type_id = localStorage.getItem("user_type_id");
   const studyOnClick = (e) => {
     e.preventDefault();
-    navigate("/Signup");
+    if (user_id) {
+      navigate("/students");
+    } else {
+      navigate("/Signup");
+    }
   };
 
   const proOnClick = (e) => {
     e.preventDefault();
-    navigate("/Signup");
+    //check if the user has logged in
+    if (user_id) {
+      console.log("hhh");
+      //now check if he is a student
+      if (user_type_id === "0") {
+        //the user is a student
+        //check if he/she is a pro or free member
+        //if pro=> navigate to tutors page
+        //if he is a free member => navigate to study page
+        axios
+          .post(`http://127.0.0.1:8000/api/student/getStudentById`, {
+            user_id: user_id,
+          })
+          .then((res) => {
+            let account_type = res.data.student[0].account_type;
+            if (account_type === "0") {
+              //free memeber
+              navigate("/study");
+            } else {
+              //pro member
+              navigate("/teachers");
+            }
+          });
+      }
+    } else {
+      navigate("/Signup");
+    }
   };
   {
     if (user_id) {
